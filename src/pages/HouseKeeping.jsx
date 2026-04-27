82f9786fc296203e91635c995173399ea33371b2
NrBrT <norbert.toth83@gmail.com>
2026-04-27 20:29:54 +0200

    continue rework

--- src/pages/HouseKeeping.jsx
+++ src/pages/HouseKeeping.jsx
@@ -1,10 +1,11 @@
 import './HouseKeeping.css'
+import RoomCard from '../components/RoomCard.jsx'
 
 export default function HouseKeeping({rooms}) {
     return ( <>
         <div id="content-header">
             <div>
-                <h2>Room Management</h2>
+                <h2>Szobák menedzselése</h2>
                 <p>48 kiadott szoba követése</p>
             </div>
             <div>
@@ -14,19 +15,7 @@ export default function HouseKeeping({rooms}) {
         </div>
         <div id="hk-content">
             {rooms.map((room) => (
-                <div className="room-card" key={room.number}>
-                    <div className=" card-row row1">
-                        <h3 className="room-number">{room.number}</h3>
-                        <div className="room-type">{room.type}</div>
-                        <i className="fa-solid fa-circle"></i>
-                    </div>
-                    <div className=" card-row row2">
-                        <div className="infopiece">Ready for Guest</div>
-                    </div>
-                    <div className=" card-row row3">
-                        <div className="room-status"><button className="btn-primary">Quick book</button></div>
-                    </div>
-                </div>
+                <RoomCard key={room.number} room={room}></RoomCard>
             ))}
         </div>
         <div id="content-footer"></div>
